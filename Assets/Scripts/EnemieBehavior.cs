0e4de3d33e08254dc05e5f233297608570b296cf
Alinto <AntonLisborg@gmail.com>
2025-05-27 10:24:51 +0200

    ..

--- Assets/Scripts/EnemieBehavior.cs
+++ Assets/Scripts/EnemieBehavior.cs
@@ -1,9 +1,17 @@
+using System.Collections.Generic;
+using TMPro;
+using Unity.Mathematics;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.AI;
 
 public class EnemieBehavior : MonoBehaviour
 {
     [SerializeField] Transform Target;
+    [SerializeField] int health;
+
+    [Header("Effects")]
+    [SerializeField] List<GameObject> deathFxPrefabs;
 
     [Header("Animations")]
     [SerializeField] Animator animator;
@@ -21,11 +29,41 @@ public class EnemieBehavior : MonoBehaviour
         if (animator == null) Debug.LogError($"No animator assigned in enemy script, {name}");
 
         animator.CrossFade(walkAnimName, 0.2f);
-    }
 
+    }
 
     void Update()
     {
         agent.SetDestination(Target.position);
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        Bullet bullet = other.gameObject.GetComponent<Bullet>();
+
+        if (bullet == null) return;
+
+        ApplyDamage(bullet.damage);
+    }
+
+    public void ApplyDamage(int _dmg)
+    {
+        if (_dmg <= 0) return;
+
+        health -= _dmg;
+        if (health <= 0)
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        foreach (GameObject _fx in deathFxPrefabs)
+        {
+            Instantiate(_fx);
+        }
+        Destroy(gameObject);
+        gameObject.SetActive(false);
     }
 }
